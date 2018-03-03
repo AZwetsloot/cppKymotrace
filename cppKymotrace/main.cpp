5150531b9d9fa3a968caa2be16011178e709c4ee
alex <alex@zwetsloot.uk>
2018-03-03 11:42:13 +0000

    Initial Commit

---
+++ cppKymotrace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppKymotrace
+//
+//  Created by Alex Zwetsloot on 03/03/2018.
+//  Copyright © 2018 Alex Zwetsloot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
